4ce2a0b0a48d8ff246a35ef23e6bdbd1a0376fad
unknown <coderanirudh@gmail.com>
2023-08-25 23:18:22 +0530

    testing transfer 3

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -10,7 +10,7 @@ const navigation = [
   { name: "Product", href: "/sign-in" },
   { name: "Features", href: "#" },
   { name: "Pricing", href: "#" },
-  { name: "Us", href: "#" },
+  { name: "About", href: "#" },
 ];
 
 const Navbar = async () => {
