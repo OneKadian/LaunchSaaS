b8f159d45b2c7ed9b87dd170879fb715c576751c
unknown <coderanirudh@gmail.com>
2023-09-15 22:05:26 +0530

    sidemenu border fixed

--- app/components/SideMenu.jsx
+++ app/components/SideMenu.jsx
@@ -8,7 +8,7 @@ import Logo from "../Images/logo.png";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Product", href: "/sign-in" },
+  { name: "Product", href: "#" },
   { name: "Pricing", href: "#" },
   { name: "About", href: "#" },
 ];
@@ -34,12 +34,12 @@ const SideMenu = () => {
       </div>
       <Dialog
         as="div"
-        className="lg:hidden"
+        className="lg:hidden "
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-3/5 overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 border-l border-gray-800 right-0 z-50 w-3/5 overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
@@ -55,13 +55,13 @@ const SideMenu = () => {
             </button>
           </div>
           <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
+            <div className="-my-6 divide-y divide-gray-500/10 ">
+              <div className="space-y-2 py-6 ">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:text-indigo-600"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:text-white"
                   >
                     {item.name}
                   </a>
