77649202e308aa53c31218982e6507e47fb997d7
unknown <coderanirudh@gmail.com>
2023-09-23 21:27:53 +0530

    altered CTA margins

--- app/components/Important/CallToAction.jsx
+++ app/components/Important/CallToAction.jsx
@@ -1,47 +1,44 @@
 export default function CallToAction() {
   // Call to Action section
   return (
-    <div className="bg-gray-900 py-24 lg:py-32 ">
-      <div className="mx-auto sm:px-6 lg:px-8 ">
-        <div className="relative overflow-hidden bg-gray-900 px-6 pt-16  sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-          {/* Text section */}
-          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-            <h2 className="text-3xl font-bold tracking-tight text-white">
-              Boost your productivity.
-              <br />
-              Start using our app today.
-            </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
-              Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
-              Malesuada adipiscing sagittis vel nulla.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-              <a
-                href="#"
-                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-              >
-                Get started
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-white"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
+    // <div className="bg-gray-900 py-24 lg:py-32 ">
+    <div className="mx-auto sm:px-6 lg:px-8 bg-gray-900 py-24 lg:py-32 ">
+      <div className="relative overflow-hidden bg-gray-900 px-6 sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+        {/* Text section */}
+        <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+          <h2 className="text-3xl font-bold tracking-tight text-white">
+            Boost your productivity.
+            <br />
+            Start using our app today.
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
+            Malesuada adipiscing sagittis vel nulla.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+            <a
+              href="#"
+              className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            >
+              Get started
+            </a>
+            <a href="#" className="text-sm font-semibold leading-6 text-white">
+              Learn more <span aria-hidden="true">→</span>
+            </a>
           </div>
-          {/* Image section */}
-          <div className="relative mt-16 h-80 lg:mt-8">
-            <img
-              className="absolute left-0 top-0 w-[50rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-              alt="App screenshot"
-              width={1824}
-              height={1080}
-            />
-          </div>
+        </div>
+        {/* Image section */}
+        <div className="relative mt-16 h-80 lg:mt-8">
+          <img
+            className="absolute left-0 top-0 w-[50rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+            alt="App screenshot"
+            width={1824}
+            height={1080}
+          />
         </div>
       </div>
     </div>
+    // </div>
   );
 }
