a1058f82869aa50b94e1fe379ee8c73b124a8b4d
unknown <coderanirudh@gmail.com>
2023-11-10 14:18:40 +0530

    running env keys

--- supabase/index.js
+++ supabase/index.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-
+// Put supabase key and URL
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
